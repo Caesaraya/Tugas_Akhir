cf08aaea026328ede6267f2e1607f5d748acec55
XOsmar969304 <osmarbaba234@gmail.com>
2026-08-18 13:27:46 +0700

    update kasir dashboard mobile

--- lib/widget/widget mobile/dashboard/greeting.dart
+++ lib/widget/widget mobile/dashboard/greeting.dart
@@ -7,21 +7,7 @@ class DashboardGreeting extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Halo Someone!  ',
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-            color: Colors.black87,
-          ),
-        ),
-        SizedBox(height: 2),
-        Text(
-          'Mau pesan apa hari ini?',
-          style: TextStyle(color: Colors.grey, fontSize: 13),
-        ),
-      ],
+      children: [],
     );
   }
 }
